c5a4f68743199de8249396fe264b505fc9de1113
M.Hammad <hammmadakhter@gmail.com>
2023-09-01 17:41:56 +0500

    update dashboard.js
    updated dashboard

--- dashboard.js
+++ dashboard.js
@@ -1,29 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const blogForm = document.getElementById("blogForm");
-    const publishBtn = document.getElementById("publishBtn");
-    const blogPosts = document.getElementById("blogPosts");
-  
-    publishBtn.addEventListener("click", function () {
-      const titleInput = document.getElementById("title");
-      const descriptionInput = document.getElementById("description");
-  
-      const title = titleInput.value;
-      const description = descriptionInput.value;
-      const date = new Date();
-  
-      if (title && description) {
-        const blogBox = document.createElement("div");
-        blogBox.classList.add("blog-box");
-        blogBox.innerHTML = `<h2>Title : ${title}</h2><p>Description : ${description}</p>`;
-        blogPosts.appendChild(blogBox);
-        
-  
-        titleInput.value = "";
-        descriptionInput.value = "";
-      }
-      else{
-        alert("please add some words")
-      }
-    });
+  const blogForm = document.getElementById("blogForm");
+  const publishBtn = document.getElementById("publishBtn");
+  const blogPosts = document.getElementById("blogPosts");
+
+  publishBtn.addEventListener("click", function () {
+    const titleInput = document.getElementById("title");
+    const descriptionInput = document.getElementById("description");
+
+    const title = titleInput.value;
+    const description = descriptionInput.value;
+    const date = new Date();
+
+    if (title && description) {
+      const blogBox = document.createElement("div");
+      blogBox.classList.add("blog-box");
+      blogBox.innerHTML = `<h2>Title : ${title}</h2><p>Description : ${description}</p>`;
+      blogPosts.appendChild(blogBox);
+      
+
+      titleInput.value = "";
+      descriptionInput.value = "";
+    }
+    else{
+      alert("please add some words")
+    }
   });
-  
+});
